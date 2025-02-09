14daae57cc50adc4eadd42d5dc599fc78638d542
Ju Maronez <maronez.juliana@gmail.com>
2025-02-09 11:07:25 -0400

    começando layout de pagina

---
+++ src/components/Header/Banner/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Container} from "./styles";
+
+const Banner = () => {
+    return(
+        <Container>
+            <Text>
+                
+            </Text>
+        </Container>
+    )
+}
+
+export default Banner;
